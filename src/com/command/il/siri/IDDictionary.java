428127c577e7b57ea24c1f8c16ad41c8b15e37cb
Noam-PC <noam@noamsmadar.com>
2019-06-28 15:08:43 +0300

    Continueing work on IDD

--- src/com/command/il/siri/IDDictionary.java
+++ src/com/command/il/siri/IDDictionary.java
@@ -11,7 +11,8 @@ public class IDDictionary {
 	public IDDictionary() {
 		enumNameMap = new HashMap<>();
 		
-		// Resource Blocks
+		// Mineral Blocks
+		
 		String[] diamondBlock = {"block of diamond", "diamond block", "blocks of diamond", "diamond blocks"};
 		String[] emeraldBlock = {"block of emerald", "emerald block", "blocks of emerald", "emerald blocks"};
 		String[] goldBlock = {"block of gold", "gold block", "blocks of gold", "gold blocks"};
@@ -19,6 +20,7 @@ public class IDDictionary {
 		String[] coalBlock = {"block of coal", "coal block", "blocks of coal", "coal blocks"};
 		String[] lapisBlock = {"block of lapis", "lapis block", "blocks of lapis", "lapis blocks", "block of lapis lazuli", "lapis lazuli block", "blocks of lapis lazuli", "lapis lazuli blocks"};
 		String[] redstoneBlock = {"block of redstone", "redstone block", "blocks of redstone", "redstone blocks", "block of red stone", "red stone block", "blocks of red stone", "red stone blocks"};
+		String[] quartzBlock = {"block of quartz", "quartz block", "blocks of quartz", "quartz blocks"};
 		
 		enumNameMap.put(diamondBlock, "diamond_block");
 		enumNameMap.put(emeraldBlock, "emerald_block");
@@ -27,8 +29,10 @@ public class IDDictionary {
 		enumNameMap.put(coalBlock, "coal_block");
 		enumNameMap.put(lapisBlock, "lapis_block");
 		enumNameMap.put(redstoneBlock, "redstone_block");
+		enumNameMap.put(quartzBlock, "quartz_block");
 		
 		// Tools
+		
 		String[] woodAxe = {"wooden axe", "wood axe"};
 		String[] stoneAxe = {"stone axe"};
 		String[] ironAxe = {"iron axe"};
@@ -57,6 +61,8 @@ public class IDDictionary {
 		String[] flintAndSteel = {"flint and steel", "lighter"};
 		String[] fishingRod = {"fishing rod"};
 		String[] carrotOnAStick = {"carrot on a stick"};
+		String[] totemOfUndying = {"totem"};
+		String[] debugStick = {"debug stick"};
 		
 		enumNameMap.put(woodAxe, "wooden_axe");
 		enumNameMap.put(stoneAxe, "stone_axe");
@@ -86,8 +92,91 @@ public class IDDictionary {
 		enumNameMap.put(flintAndSteel, "flint_and_steel");
 		enumNameMap.put(fishingRod, "fishing_rod");
 		enumNameMap.put(carrotOnAStick, "carrot_on_a_stick");
+		enumNameMap.put(totemOfUndying, "totem_of_undying");
+		enumNameMap.put(debugStick, "debug_stick");
+		
+		// Armor
+		
+		String[] leatherBoots = {"leather boot", "leather shoe"};
+		String[] chainBoots = {"chain boot", "chain shoe", "chainmail boot", "chainmail shoe"};
+		String[] ironBoots = {"iron boot", "iron shoe"};
+		String[] goldBoots = {"golden boot", "gold boot", "golden shoe", "gold shoe"};
+		String[] diamondBoots = {"diamond boot"};
+		String[] leatherLeggings = {"leather legging", "leather pants"};
+		String[] chainLeggings = {"chain legging", "chain pants", "chainmail legging", "chainmail pants"};
+		String[] ironLeggings = {"iron legging", "iron pants"};
+		String[] goldLeggings = {"golden legging", "golden pants", "gold legging", "gold pants"};
+		String[] diamondLeggings = {"diamond legging", "diamond pants"};
+		String[] leatherChestplate = {"leather chestplate", "leather tunic", "leather shirt"};
+		String[] chainChestplate = {"chain chestplate", "chain shirt", "chainmail chestplate", "chainmail shirt"};
+		String[] ironChestplate = {"iron chestplate", "iron shirt"};
+		String[] goldChestplate = {"golden chestplate", "golden shirt", "gold chestplate", "gold shirt"};
+		String[] diamondChestplate = {"diamond chestplate", "diamond shirt"};
+		String[] leatherHelmet = {"leather helmet", "leather hat"};
+		String[] chainHelmet = {"chain helmet", "chain hat", "chainmail helmet", "chainmail hat"};
+		String[] ironHelmet = {"iron helmet", "iron hat"};
+		String[] goldHelmet = {"golden helmet", "golden hat", "gold helmet", "gold hat"};
+		String[] diamondHelmet = {"diamond helmet", "diamond hat"};
+		String[] leatherHorseArmor = {"leather horse armor"};
+		String[] ironHorseArmor = {"iron horse armor"};
+		String[] goldHorseArmor = {"golden horse armor", "gold horse armor"};
+		String[] diamondHorseArmor = {"diamond horse armor"};
+		String[] turtleShell = {"turtle shell", "turtle helmet", "turtle hat"};
+		
+		enumNameMap.put(leatherBoots, "leather_boots");
+		enumNameMap.put(chainBoots, "chainmail_boots");
+		enumNameMap.put(ironBoots, "iron_boots");
+		enumNameMap.put(goldBoots, "golden_boots");
+		enumNameMap.put(diamondBoots, "diamond_boots");
+		enumNameMap.put(leatherLeggings, "leather_leggings");
+		enumNameMap.put(chainLeggings, "chainmail_leggings");
+		enumNameMap.put(ironLeggings, "iron_leggings");
+		enumNameMap.put(goldLeggings, "golden_leggings");
+		enumNameMap.put(diamondLeggings, "diamond_leggings");
+		enumNameMap.put(leatherChestplate, "leather_chestplate");
+		enumNameMap.put(chainChestplate, "chainmail_chestplate");
+		enumNameMap.put(ironChestplate, "iron_chestplate");
+		enumNameMap.put(goldChestplate, "gold_chestplate");
+		enumNameMap.put(diamondChestplate, "diamond_chestplate");
+		enumNameMap.put(leatherHelmet, "leather_helmet");
+		enumNameMap.put(chainHelmet, "chainmail_helmet");
+		enumNameMap.put(ironHelmet, "iron_helmet");
+		enumNameMap.put(goldHelmet, "golden_helmet");
+		enumNameMap.put(diamondHelmet, "diamond_helmet");
+		enumNameMap.put(leatherHorseArmor, "leather_horse_armor");
+		enumNameMap.put(ironHorseArmor, "iron_horse_armor");
+		enumNameMap.put(goldHorseArmor, "golden_horse_armor");
+		enumNameMap.put(diamondHorseArmor, "diamond_horse_armor");
+		enumNameMap.put(turtleShell, "turtle_helmet");
+		
+		// Vehicles
+		
+		String[] birchBoat = {"birch boat"};
+		String[] spruceBoat = {"spruce boat"};
+		String[] acaciaBoat = {"acacia boat"};
+		String[] jungleBoat = {"jungle boat", "jungle wood boat"};
+		String[] darkOakBoat = {"dark boat", "dark oak boat"};
+		String[] oakBoat = {"boat"};
+		String[] furnaceMinecart = {"furnace minecart", "furnace cart", "minecart with furnace", "minecart with a furnace"};
+		String[] chestMinecart = {"chest minecart", "chest cart", "minecart with chest", "minecart with a chest"};
+		String[] tntMinecart = {"tnt minecart", "tnt cart", "minecart with tnt"};
+		String[] hopperMinecart = {"hopper minecart", "hopper cart", "minecart with hopper", "minecart with a hopper"};
+		String[] commandBlockMinecart = {"command block minecart", "command block cart", "minecart with command block", "minecart with a command block"};
+		
+		enumNameMap.put(birchBoat, "birch_boat");
+		enumNameMap.put(spruceBoat, "spruce_boat");
+		enumNameMap.put(acaciaBoat, "acacia_boat");
+		enumNameMap.put(jungleBoat, "jungle_boat");
+		enumNameMap.put(darkOakBoat, "dark_oak_boat");
+		enumNameMap.put(oakBoat, "oak_boat");
+		enumNameMap.put(furnaceMinecart, "furnace_minecart");
+		enumNameMap.put(chestMinecart, "chest_minecart");
+		enumNameMap.put(tntMinecart, "tnt_minecart");
+		enumNameMap.put(hopperMinecart, "hopper_minecart");
+		enumNameMap.put(commandBlockMinecart, "command_block_minecart");
 		
 		// Food
+		
 		String[] goldenApple = {"golden apple", "gold apple", "gapple"};
 		String[] enchantedGoldenApple = {"enchanted golden apple", "enchanted gold apple", "notch apple"};
 		String[] beetrootSoup = {"beetroot soup", "beetroot stew"};
@@ -221,6 +310,7 @@ public class IDDictionary {
 		String[] glisteringMelon = {"glistering melon"};
 		String[] netherBrick = {"nether brick"};
 		String[] poppedChorusFruit = {"popped chorus"};
+		String[] fireCharge = {"fire charge", "fireball", "fire ball"};
 		
 		enumNameMap.put(nameTag, "nametag");
 		enumNameMap.put(blazePowder, "blaze_powder");
@@ -231,6 +321,7 @@ public class IDDictionary {
 		enumNameMap.put(glisteringMelon, "glistering_melon");
 		enumNameMap.put(netherBrick, "nether_brick");
 		enumNameMap.put(poppedChorusFruit, "popped_chorus_fruit");
+		enumNameMap.put(fireCharge, "fire_charge");
 		
 		// Dyes
 		
@@ -251,7 +342,50 @@ public class IDDictionary {
 		String[] blackDye = {"black dye", "ink dye"};
 		String[] brownDye = {"brown dye"};
 		
+		enumNameMap.put(roseRed, "red_dye");
+		enumNameMap.put(pinkDye, "pink_dye");
+		enumNameMap.put(orangeDye, "orange_dye");
+		enumNameMap.put(dandelionYellow, "yellow_dye");
+		enumNameMap.put(cactusGreen, "green_dye");
+		enumNameMap.put(limeDye, "lime_dye");
+		enumNameMap.put(lightBlueDye, "light_blue_dye");
+		enumNameMap.put(cyanDye, "cyan_dye");
+		enumNameMap.put(blueDye, "blue_dye");
+		enumNameMap.put(magentaDye, "magenta_dye");
+		enumNameMap.put(purpleDye, "purple_dye");
+		enumNameMap.put(grayDye, "gray_dye");
+		enumNameMap.put(lightGrayDye, "light_gray_dye");
+		enumNameMap.put(whiteDye, "white_dye");
+		enumNameMap.put(blackDye, "black_dye");
+		enumNameMap.put(brownDye, "brown_dye");
 		
+		// Music Discs
+		
+		String[] disc11 = {"disc 11", "record 11"};
+		String[] disc13 = {"disc 13", "record 13"};
+		String[] discCat = {"disc cat", "record cat"};
+		String[] discBlocks = {"disc blocks", "disc blocks"};
+		String[] discChirp = {"disc chirp", "record chirp"};
+		String[] discFar = {"disc far", "record far"};
+		String[] discMall = {"disc mall", "record mall"};
+		String[] discMellohi = {"disc mellohi", "record mellohi"};
+		String[] discStal = {"disc stal", "record stal"};
+		String[] discStrad = {"disc stard", "record stard"};
+		String[] discWard = {"disc ward", "record ward"};
+		String[] discWait = {"disc", "record"};
+		
+		enumNameMap.put(disc11, "music_disc_11");
+		enumNameMap.put(disc13, "music_disc_13");
+		enumNameMap.put(discCat, "music_disc_cat");
+		enumNameMap.put(discBlocks, "music_disc_blocks");
+		enumNameMap.put(discChirp, "music_disc_chirp");
+		enumNameMap.put(discFar, "music_disc_far");
+		enumNameMap.put(discMall, "music_disc_mall");
+		enumNameMap.put(discMellohi, "music_disc_mellohi");
+		enumNameMap.put(discStal, "music_disc_stal");
+		enumNameMap.put(discStrad, "music_disc_strad");
+		enumNameMap.put(discWard, "music_disc_ward");
+		enumNameMap.put(discWait, "music_disc_wait");
 		
 	}
 	
@@ -262,7 +396,8 @@ public class IDDictionary {
 		for( String[] names : enumNameMap.keySet() ) {
 			
 			for( int i = 0 ; i<names.length ; i++ ) {
-				newMsg = newMsg.replace(names[i], enumNameMap.get(names));
+				newMsg = newMsg.replace(" " + names[i] + " ", " " + enumNameMap.get(names) + " ");
+				newMsg = newMsg.replace(" " + names[i] + "s ", " " + enumNameMap.get(names) + " ");
 			}
 			
 		}
